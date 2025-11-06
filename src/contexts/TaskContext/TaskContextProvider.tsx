ce96cb5721215c7acca4a10e96f11c48a3daf1c6
Pedro Luca Simionato <plsimionato@gmail.com>
2025-11-06 20:46:38 -0300

    Adicionando workers

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -2,6 +2,7 @@ import { useEffect, useReducer } from "react";
 import { initialTaskState } from "./initialTaskState";
 import { TaskContext } from "./TaskContext";
 import { taskReducer } from "./taskReducer";
+import { TimerWorkerManager } from "../../workers/TimerWorkerManager";
 
 type TaskContextProviderProps = {
   children: React.ReactNode;
@@ -10,9 +11,18 @@ type TaskContextProviderProps = {
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
   const [state, dispatch] = useReducer(taskReducer, initialTaskState);
 
+  const worker = TimerWorkerManager.getInstance();
+
+  worker.onmessage((e) => {
+    console.log(e.data);
+  });
+
   useEffect(() => {
-    console.log(state);
-  }, [state]);
+    if (!state.activeTask) {
+      console.log("Worker terminado");
+      worker.terminate();
+    }
+  }, [worker, state]);
 
   return (
     <TaskContext.Provider value={{ state, dispatch }}>
